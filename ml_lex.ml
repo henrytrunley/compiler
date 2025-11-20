72379000299a5b83c152309e3338d25cfd53ca4e
Henry Trunley <hjtrunley@gmail.com>
2025-11-20 16:30:18 +0000

    put arg parsing in func

--- ml_lex.ml
+++ ml_lex.ml
@@ -1,19 +1,21 @@
 (* open Stdio *)
 (* open Base *)
 
-let usage_msg = "ml_lex </path/to/input_file.lex> </path/to/output_file.ml"
-let args = ref []
-let speclist = []
-let anon_fun arg = args := arg :: !args
+let parse_args () =
+    let usage_msg = "ml_lex </path/to/input_file.lex> </path/to/output_file.ml" in
+    let args = ref [] in
+    let speclist = [] in
+    let anon_fun arg = args := arg :: !args in
+    Stdlib.Arg.parse speclist anon_fun usage_msg;
+    (match !args with
+        | input_file :: output_file :: [] -> (input_file, output_file)
+        | _ -> failwith "This program takes two arguments as input"
+    )
 
 let read_file _ =
     failwith "read file"
 
 let () =
-    Stdlib.Arg.parse speclist anon_fun usage_msg;
-    let input_file = (match !args with
-        | input_file :: _ :: [] -> input_file
-        | _ -> failwith "This program takes two arguments as input"
-    ) in
+    let input_file, _ = parse_args () in
     let _ = read_file input_file in
     ()
