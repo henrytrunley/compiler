59288a03529026d652ee73e5fc84bb464863ce72
Henry Trunley <hjtrunley@gmail.com>
2025-11-20 20:30:19 +0000

    old ml_lex changes

--- ml_lex.ml
+++ ml_lex.ml
@@ -1,5 +1,5 @@
-(* open Stdio *)
-(* open Base *)
+open Stdio
+open Base
 
 let parse_args () =
     let usage_msg = "ml_lex </path/to/input_file.lex> </path/to/output_file.ml" in
@@ -8,14 +8,15 @@ let parse_args () =
     let anon_fun arg = args := arg :: !args in
     Stdlib.Arg.parse speclist anon_fun usage_msg;
     (match !args with
-        | input_file :: output_file :: [] -> (input_file, output_file)
+        | output_file :: input_file :: [] -> (input_file, output_file)
         | _ -> failwith "This program takes two arguments as input"
     )
 
-let read_file _ =
-    failwith "read file"
+let generate_lexer input_file =
+    let contents = In_channel.read_all input_file in
+    let parts = Str.split (Str.regexp "%%") contents in
+    List.iter parts ~f:(fun s -> printf "\n\nPART:\n%s" s)
 
 let () =
     let input_file, _ = parse_args () in
-    let _ = read_file input_file in
-    ()
+    generate_lexer input_file
