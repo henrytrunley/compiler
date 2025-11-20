4a07a5b9bb4afc9f90570b3bd300f82921414e3f
Henry Trunley <hjtrunley@gmail.com>
2025-11-20 22:00:44 +0000

    cleanup nfa

--- nfa.ml
+++ nfa.ml
@@ -2,23 +2,32 @@ open Stdio
 
 module IntSet = Set.Make(Base.Int)
 
-type automata = {
+type nfa = {
     trans : IntSet.t -> char -> IntSet.t;
-    accept : IntSet.t -> bool;
-    mutable states : IntSet.t;
+    accepts : IntSet.t -> bool;
+    start : IntSet.t;
 }
 
-let delta = function
-    | 1 -> (function | '0' -> IntSet.of_list[1] | '1' -> IntSet.of_list[2] | _ -> IntSet.empty )
-    | 2 -> (function | '0' -> IntSet.of_list[3] | '1' -> IntSet.of_list[2] | _ -> IntSet.empty )
-    | 3 -> (function | '0' -> IntSet.of_list[2] | '1' -> IntSet.of_list[2] | _ -> IntSet.empty )
-    | _ -> (function | _ -> IntSet.empty)
-
-let trans states c =
+let trans delta states c =
     IntSet.fold (fun state acc ->
-        let new_states = delta state c in
+        let new_states = IntSet.of_list (delta state c) in
         IntSet.union acc new_states
     ) states IntSet.empty
+
+let accept f states =
+    IntSet.exists f states
+
+let construct d f q0 = {
+    trans = trans d;
+    accepts = accept f;
+    start = IntSet.of_list q0
+}
+
+let d = function
+    | 1 -> (function | '0' -> [1] | '1' -> [2] | _ -> [] )
+    | 2 -> (function | '0' -> [3] | '1' -> [2] | _ -> [] )
+    | 3 -> (function | '0' -> [2] | '1' -> [2] | _ -> [] )
+    | _ -> (function | _ -> [])
 
 let f = function
     | 1 -> false
@@ -26,18 +35,17 @@ let f = function
     | 3 -> false
     | _ -> false
 
-let accept states =
-    IntSet.exists f states
+let q0 = [1]
 
-let q0 = IntSet.of_list[1]
 
 let run nfa input =
+    let state = ref nfa.start in
     String.iter (fun c ->
-        nfa.states <- trans nfa.states c
+        state := nfa.trans !state c
     ) input;
-    nfa.accept nfa.states
+    nfa.accepts !state
 
 let () =
-    let dfa = { trans ; accept ; states = q0 } in
-    let res = run dfa "0100" in
+    let nfa = construct d f q0 in
+    let res = run nfa "0100000000000000000000000001010101010110100" in
     printf "%b\n" res
