3c98d016e1405baeec4debb541db2f2857a9a5f3
Henry Trunley <hjtrunley@gmail.com>
2025-11-21 10:22:25 +0000

    rename to match textbook

--- nfa.ml
+++ nfa.ml
@@ -3,31 +3,38 @@ open Stdio
 module IntSet = Set.Make(Base.Int)
 
 type nfa = {
-    trans : IntSet.t -> char -> IntSet.t;
+    dfa_edge : IntSet.t -> string -> IntSet.t;
     accepts : IntSet.t -> bool;
     start : IntSet.t;
 }
 
-let trans delta states c =
+let expand edge states c =
     IntSet.fold (fun state acc ->
-        let new_states = IntSet.of_list (delta state c) in
-        IntSet.union acc new_states
+        IntSet.union acc (IntSet.of_list (edge state c))
     ) states IntSet.empty
+
+let rec closure edge states =
+    let t = IntSet.union states (expand edge states "") in
+    if IntSet.equal t states then t else closure edge t
+
+let dfa_edge edge states c =
+    let new_states = expand edge states c in
+    closure edge new_states
 
 let accept f states =
     IntSet.exists f states
 
 let construct d f q0 = {
-    trans = trans d;
+    dfa_edge = dfa_edge d;
     accepts = accept f;
-    start = IntSet.of_list q0
+    start = closure d (IntSet.of_list q0)
 }
 
-let d = function
-    | 1 -> (function | '0' -> [1] | '1' -> [2] | _ -> [] )
-    | 2 -> (function | '0' -> [3] | '1' -> [2] | _ -> [] )
-    | 3 -> (function | '0' -> [2] | '1' -> [2] | _ -> [] )
-    | _ -> (function | _ -> [])
+let edge s c = match s with
+    | 1 -> (match c with | "0" -> [1] | "1" -> [2] | _ -> [] )
+    | 2 -> (match c with | "0" -> [3] | "1" -> [2] | _ -> [] )
+    | 3 -> (match c with | "0" -> [2] | "1" -> [2] | _ -> [] )
+    | _ -> (match c with | _ -> [])
 
 let f = function
     | 1 -> false
@@ -37,15 +44,14 @@ let f = function
 
 let q0 = [1]
 
-
 let run nfa input =
     let state = ref nfa.start in
     String.iter (fun c ->
-        state := nfa.trans !state c
+        state := nfa.dfa_edge !state (String.make 1 c)
     ) input;
     nfa.accepts !state
 
 let () =
-    let nfa = construct d f q0 in
-    let res = run nfa "0100000000000000000000000001010101010110100" in
+    let nfa = construct edge f q0 in
+    let res = run nfa "010000000000000000000000000101010101011010" in
     printf "%b\n" res
