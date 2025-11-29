7aa787df852360124dc1460b11ec2bc98a021f02
Henry Trunley <hjtrunley@gmail.com>
2025-11-29 09:26:52 +0000

    get map of expanded regex to ml code

--- lexer/ml_lex.ml
+++ lexer/ml_lex.ml
@@ -19,6 +19,8 @@ let find_and_replace_all string map =
         fun string (pattern, with_) -> String.substr_replace_all string ~pattern:pattern ~with_:with_
     )
     
+let print_map map =
+    List.iter map ~f:(fun (x, y) -> printf "%s goes to %s\n" x y)
 
 let process_aliases aliases =
     let lines = String.split aliases ~on:'\n' in
@@ -30,16 +32,26 @@ let process_aliases aliases =
             | Some (pattern, with_) -> ("{" ^ pattern ^ "}", with_) :: map
     )
 
+let whitespace c = match c with
+    | ' ' | '\t' -> true
+    | _ -> false
+
 let process_map map aliases = 
     let processed_map = find_and_replace_all map aliases in
-    printf "%s" processed_map
+    let lines = String.split processed_map ~on:'\n' in
+    List.fold lines ~init:[] ~f:(
+        fun map line ->
+            match Str.bounded_split (Str.regexp "=>") line 2 with
+            | from :: to_ :: [] -> (String.rstrip ~drop:whitespace from, String.lstrip ~drop:whitespace to_) :: map
+            | _ -> map
+    )
 
 
 let generate_lexer input_file =
     let contents = In_channel.read_all input_file in
     let parts = Str.split (Str.regexp "%%") contents in
     match parts with
-        | _ :: a :: m :: [] -> let aliases = process_aliases a in let map = process_map m aliases in map
+        | _ :: a :: m :: [] -> let aliases = process_aliases a in let map = process_map m aliases in print_map map
         | _ -> failwith "Incorrectly formatted input file. 3 parts were expected, separated by %%."
 
 let () =
